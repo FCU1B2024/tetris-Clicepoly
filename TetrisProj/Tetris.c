f1efb662d64418672966b7d6d66fadc9193645ce
Clicepoly <papyruuuuu@gmail.com>
2024-05-09 12:12:14 +0800

    First_Commit

--- TetrisProj/Tetris.c
+++ TetrisProj/Tetris.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#define CANVAS_WIDTH 10
+#define CANVAS_HEIGHT 20
 
 
 typedef enum { // colors of tetris pieces
@@ -15,28 +17,36 @@ typedef enum { // colors of tetris pieces
 
 typedef enum { // shape names of tetris pieces
     SHAPE_EMPTY = -1,
-    SHAPE_I, 
+    SHAPE_I,
     SHAPE_J, // mirror L
     SHAPE_L,
     SHAPE_O,
-    SHAPE_S, 
+    SHAPE_S,
     SHAPE_T,
-    SHAPE_Z  // mirror S
+    SHAPE_Z, // mirror S
+    SHAPE_X,  // cursed
+    HUGE_L
 }ShapeId;
+
+typedef struct {
+    Color color;
+    ShapeId shape;
+    bool current;
+}Block;
 
 typedef struct {
     ShapeId shape;
     Color color;
     int size;
-    char rotates[4][4][4];
+    char rotates[6][6][6];
 }Shape;
 
-Shape shape[7] = 
+Shape shape[9] = // 0~7 normal
 {
     {
-        .shape  = SHAPE_I,
-        .color  = COLOR_CYAN,
-        .size   = 4,
+        .shape = SHAPE_I,
+        .color = COLOR_CYAN,
+        .size = 4,
         .rotates = {
             {
                 {0,0,0,0},
@@ -89,8 +99,8 @@ Shape shape[7] =
                 {0,0,1},
                 {0,0,0}
             }
-            
-            
+
+
         }
     },
     {
@@ -173,7 +183,7 @@ Shape shape[7] =
 
 
         }
-    }, 
+    },
     {
         .shape = SHAPE_S,
         .color = COLOR_RED,
@@ -207,7 +217,7 @@ Shape shape[7] =
         .color = COLOR_GREEN,
         .size = 3,
         .rotates = {
-            
+
             {
                 {0,0,0},
                 {1,1,0},
@@ -219,14 +229,14 @@ Shape shape[7] =
                 {0,1,0}
 
             },
-            
+
             {
                 {1,1,0},
                 {0,1,1},
                 {0,0,0}
-                
+
             },
-            
+
             {
                 {0,1,0},
                 {1,1,0},
@@ -235,40 +245,157 @@ Shape shape[7] =
 
 
         }
-    }
+    },  {
+
+
+
+        // cursed pieces section
+
+            .shape = SHAPE_X,
+            .color = COLOR_YELLOW,
+            .size = 3,
+            .rotates = {
+                {
+                    {1,0,1},
+                    {0,1,0},
+                    {1,0,1}
+                },
+                {
+                    {1,0,1},
+                    {0,1,0},
+                    {1,0,1}
+                },
+                {
+                    {1,0,1},
+                    {0,1,0},
+                    {1,0,1}
+                },
+                {
+                    {1,0,1},
+                    {0,1,0},
+                    {1,0,1}
+                }
+
+
+            }
+        }, {
+            .shape = HUGE_L,
+            .color = COLOR_YELLOW,
+            .size = 6,
+            .rotates = {
+                {
+                    {1,1,0,0,0,0},
+                    {1,1,0,0,0,0},
+                    {1,1,0,0,0,0},
+                    {1,1,0,0,0,0},
+                    {1,1,1,1,0,0},
+                    {1,1,1,1,0,0}
+                },
+                {
+                    {0,0,0,0,0,0},
+                    {0,0,0,0,0,0},
+                    {0,0,0,0,1,1},
+                    {0,0,0,0,1,1},
+                    {1,1,1,1,1,1},
+                    {1,1,1,1,1,1}
+                },
+                {
+                    {0,0,1,1,1,1},
+                    {0,0,1,1,1,1},
+                    {0,0,0,0,1,1},
+                    {0,0,0,0,1,1},
+                    {0,0,0,0,1,1},
+                    {0,0,0,0,1,1}
+                },
+                {
+                    {1,1,1,1,1,1},
+                    {1,1,1,1,1,1},
+                    {1,1,0,0,0,0},
+                    {1,1,0,0,0,0},
+                    {0,0,0,0,0,0},
+                    {0,0,0,0,0,0}
+                }
+
+
+            }
+        }
+
 };
 
+void setBlock(Block* block, Color color, ShapeId shape, bool current) {
+    block->color = color;
+    block->shape = shape;
+    block->current = current;
+}
+void resetBlock(Block* block) {
+    block->color = COLOR_BLACK;
+    block->shape = SHAPE_EMPTY;
+    block->current = false;
+}
+
+typedef struct {
+    int x;
+    int y;
+    int score;
+    int rotate;
+    int fallTime;
+    ShapeId queue[4];
+}State;
+
+
+
 int main() {
-    
-    Color cur;
+
+    //Color cur;
     // how many shapes
-    for (int i = 0; i < 7; i++) {
+    Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+    State state = {
+        .x = CANVAS_HEIGHT / 2,
+        .y = 0,
+        .score = 0,
+        .rotate = 0,
+        .fallTime = 0
+    };
 
-        // rotations
-        for (int r = 0; r < 4; r++) {
 
-            // prints the shape
-            // what kind
-            for (int s = 0; s < shape[i].size; s++) {
-                // 2d
-                for (int t = 0; t < shape[i].size; t++) {
-                    if (shape[i].rotates[r][s][t] == 0) {
-                        cur = COLOR_WHITE;
 
-                    }
-                    else {
-                        cur = shape[i].color;
 
-                    }
-                    printf("\033[%dm \033[0m", cur);
-                    printf("\033[%dm \033[0m", cur);
-                }
-                printf("\n");
-            }
-            
-            printf("\n");
+
+
+    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+        for (int j = 0; j < CANVAS_WIDTH; j++) {
+            resetBlock(&canvas[i][j]);
         }
-        
     }
+
+    Shape tmp = shape[1];
+
+    for (int i = 0; i < tmp.size; i++) {
+        for (int j = 0; j < tmp.size; j++) {
+            if (tmp.rotates[1][i][j] == 1) {
+
+                setBlock(&canvas[i + state.y][j + state.x], tmp.color, tmp.shape, 1);
+
+            }
+        }
+    }
+
+    printf("\033[0:0H\n");
+    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+        printf("|");
+        for (int j = 0; j < CANVAS_WIDTH; j++) {
+            printf("\033[%dm\u3000", canvas[i][j].color);
+        }
+        printf("\033[%0m");
+        printf("|\n");
+    }
+
+
+
+
+
+
+
+
     return 0;
 }
